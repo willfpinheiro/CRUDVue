55277c7022050c361b0b298b3c18c22dc3805a53
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-12-16 21:14:41 -0300

    feat criado cabecalho

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Cabecalho></Cabecalho>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cabecalho from './components/Cabecalho.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cabecalho,
   }
 }
 </script>
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,64 @@
+<template>
+  <header>
+			<!-- Inicio cabeçalho -->
+			<nav class="navbar navbar-expand-sm navbar-light bg-primary">
+				<div class="container">
+					<!-- logo, classe serve para remover o link e definir como logo -->
+					<a href="#" class="navbar-brand">
+						<img
+							src="../assets/web-design-library.png"
+							width="50"
+							height="50"
+						/>
+						Crud
+					</a>
+
+					<button
+						class="navbar-toggler"
+						data-toggle="collapse"
+						data-target="#nav-principal"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="nav-principal">
+						<!-- navegaçao -->
+						<!-- Ao usar margem esquerda(ML), ele joga automatico para direita -->
+						<ul class="navbar-nav ml-auto">
+							<li class="nav-item">
+								<a href="#" class="nav-link">Pessoa</a>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">Estado</a>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">Endereço</a>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">Contato</a>
+							</li>
+							<li class="nav-item">
+								<a href="#" class="nav-link">Cidade</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
+</template>
+
+<script>
+
+export default {
+  name: 'Cabecalho',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+
+</style>
